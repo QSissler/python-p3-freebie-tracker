11810a0484d1b9c302458cf17a996b527ce68b95
Quinn Sissler <QSissler@gmail.com>
2023-02-21 21:19:39 -0500

    completed challenge

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, ForeignKey)
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base()
+
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -12,8 +21,29 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', backref='company')
+    devs = association_proxy('freebies', 'dev',
+        creator=lambda dv: Freebie(dev=dv))
+
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return f'<Company {self.name}, {self.founding_year}>'
+
+    def give_freebie(self, dev, item_name, value):
+        new_freebie = Freebie(
+            item_name = item_name,
+            value = value,
+            company_id = self.id,
+            dev_id = dev.id
+            )
+        session.add(new_freebie)
+        session.commit()
+        return new_freebie
+
+    @classmethod
+    def oldest_company(cls):
+        oldest_company = session.query(cls).order_by('founding_year')[0]
+        return oldest_company
+
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -21,5 +51,42 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship('Freebie', backref='dev')
+    companies = association_proxy('freebies', 'company',
+        creator=lambda cm: Freebie(company=cm))
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    def recieved_one(self, item_name):
+       for freebie in self.freebies:
+        if item_name == freebie.item_name:
+            return True
+        else:
+            return False
+    
+    def give_away(self, dev, freebie):
+        if freebie in self.freebies:
+            session.query(Freebie).filter(Freebie.id == freebie.id).update(
+                {Freebie.dev_id: dev.id})
+            session.commit()
+        
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name} {self.value}>'
+
+    def print_details(self):
+       return f'{self.dev.name} owns a {self.item_name} from {self.company.name}.'
+
+
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,76 @@
 #!/usr/bin/env python3
+from faker import Faker
+import random
+from random import choice as rc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 # Script goes here!
+fake = Faker()
+print("Seeding companies...")
+from models import Company
+
+def create_companies(): 
+    companies = [
+        Company(
+            name=fake.company(),
+            founding_year=random.randint(1950, 2022)
+        )
+        for i in range(50)]
+    session.add_all(companies)
+    session.commit()
+    return companies
+
+print("Seeding companies...")
+from models import Dev
+
+def create_devs():
+    devs = [
+         Dev(
+         name=fake.company(),
+    )
+        for i in range(50)]
+    session.add_all(devs)
+    session.commit()
+    return devs
+
+print("Seeding freebies...")
+from models import Freebie
+
+def create_freebies():
+    freebies = [
+       Freebie(
+            item_name=fake.name(),
+            value=random.randint(1, 10)
+    )
+        for i in range(100)]
+    session.add_all(freebies)
+    session.commit()
+    return freebies
+
+def delete_records():
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+    session.commit()
+
+def relate_one_to_many(companies, devs, freebies):
+    for freebie in freebies:
+        freebie.company = rc(companies)
+        freebie.dev = rc(devs)
+
+    session.add_all(freebies)
+    session.commit()
+    return companies, devs, freebies
+
+if __name__ == '__main__':
+    delete_records()
+    companies = create_companies()
+    devs = create_devs()
+    freebies = create_freebies()
+    relate_one_to_many(companies, devs, freebies)
+
